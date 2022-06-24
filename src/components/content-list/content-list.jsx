43ca79e2f210e05a2aaf4ee53513ae7e668cdc25
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-06-24 16:06:28 -0400

    Commiting shell of comps

---
+++ src/components/content-list/content-list.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./content-list.module.scss";
+
+const ContentList = () => <div>ContentList</div>;
+
+export { ContentList };
---
+++ src/components/split-screen/split-screen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./split-screen.module.scss";
+
+const SplitScreen = () => <div>SplitScreen</div>;
+
+export { SplitScreen };
